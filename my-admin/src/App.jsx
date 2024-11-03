e734442ab50f50fd30259e15c9c9ae49af5bcd11
Ndumiso Hlophe <140718594+ndumisomatthew@users.noreply.github.com>
2024-11-03 18:38:36 +0000

    GGMU

--- my-admin/src/App.jsx
+++ my-admin/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Admin, Resource, ListGuesser, PrevNextButtonClasses } from "react-admin";
+import {PocketBaseProvider} from './ra-pocketbase';
 
-function App() {
-  const [count, setCount] = useState(0)
+const pbProvider = PocketBaseProvider("https://symmetrical-fishstick-pjrg966jjvpph6p6w-8090.app.github.dev/");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => (
+  <Admin 
+  dataProvider={pbProvider.dataProvider}
+  authProvider={pbProvider.authProvider}>
+    <Resource name="posts" list={ListGuesser} />
+    <Resource name="comments" list={ListGuesser} />
+  </Admin>
+);
 
-export default App
+export default App;
